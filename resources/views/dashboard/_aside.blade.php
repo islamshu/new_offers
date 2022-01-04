b52f000bf5a849fa47bc034fccbe66d2cba2b17a
islamshu12 <islamshu12@gmail.com>
2022-01-04 12:01:53 +0200

    edit url

--- resources/views/dashboard/_aside.blade.php
+++ resources/views/dashboard/_aside.blade.php
@@ -479,7 +479,7 @@ $lang = app()->getLocale();
        
     </li>
     <li class="menu-item  menu-item-submenu" aria-haspopup="true" data-menu-toggle="hover">
-        <a href="{{ route('promotion.index') }}" class="menu-link menu-toggle">
+        <a href="/{{ $lang }}/promotion" class="menu-link menu-toggle">
             <span class="svg-icon menu-icon">
                 <i class="fa fa-bookmark" aria-hidden="true"></i>
             </span>
