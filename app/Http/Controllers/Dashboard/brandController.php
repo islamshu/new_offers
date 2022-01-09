ecd292f50a7ac9ad897c9d1d79224c9a2b05d875
islamshu12 <islamshu12@gmail.com>
2022-01-09 17:07:44 +0200

    ee

--- app/Http/Controllers/Dashboard/brandController.php
+++ app/Http/Controllers/Dashboard/brandController.php
@@ -490,11 +490,13 @@ class brandController extends Controller
     public function country_vendor($locale, $id)
     {
         $vendorCountry = vendor_country::where('vendor_id', $id)->get();
+        $category = Vendor::with('counteire')->find(Auth::user()->ent_id)->counteire;
+        dd($category);
+
         $country_array = array();
         foreach ($vendorCountry as $count) {
             array_push($country_array, $count->country_id);
         }
-        $country = Country::find($country_array);
         return response()->view('dashboard.country.index', compact('country'));
     }
     public function city_vendor($locale, $id)
