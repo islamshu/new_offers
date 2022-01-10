624633e4d2f8862a715b590523cdd464664d42bd
islamshu12 <islamshu12@gmail.com>
2022-01-10 09:48:47 +0200

    cc

--- app/Http/Controllers/Dashboard/brandController.php
+++ app/Http/Controllers/Dashboard/brandController.php
@@ -304,6 +304,7 @@ class brandController extends Controller
                     }
                 } catch (\Exception $e) {
                     DB::rollback();
+                    dd($e);
                     return response()->json(['icon' => 'error', 'title' => 'error when insert data'], 400);
                 }
             } elseif (Auth::user()->hasRole('Admin')) {
