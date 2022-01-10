a9f1f10ce465a54f1ef58951d71d573cb386480e
islamshu12 <islamshu12@gmail.com>
2022-01-10 10:29:35 +0200

    edit telephone

--- resources/views/dashboard/vendor/edit.blade.php
+++ resources/views/dashboard/vendor/edit.blade.php
@@ -531,7 +531,7 @@
                 formData.append('terms_en', document.getElementById('terms_en').value);
             }
             if (document.getElementById('telephoone') != null) {
-                formData.append('telephoone', document.getElementById('terms_en').value);
+                formData.append('telephoone', document.getElementById('telephoone').value);
             }
 
 
