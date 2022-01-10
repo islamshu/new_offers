fe835f87834e95b6a080f7384e494f76eb35af63
islamshu12 <islamshu12@gmail.com>
2022-01-10 10:16:30 +0200

    asd

--- app/Http/Controllers/Dashboard/brandController.php
+++ app/Http/Controllers/Dashboard/brandController.php
@@ -561,7 +561,7 @@ class brandController extends Controller
     public function update_brand(Request $request, $locale, $id)
     {
         $user = User::where('vendor_id', $id)->first();
-        $vendor = Vendor::find($id);
+        $vendor = Vendor::where('id',$id)->first();
         dd($user,$id);
        
         $validator = Validator($request->all(), [
