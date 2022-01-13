d24b26d68804ef7a178d9d59e63fd47ae713da8c
islamshu12 <islamshu12@gmail.com>
2022-01-13 15:14:07 +0200

    ddd

--- app/Http/Controllers/Dashboard/SubscriptionController.php
+++ app/Http/Controllers/Dashboard/SubscriptionController.php
@@ -64,7 +64,7 @@ class SubscriptionController extends Controller
     
     public function store(Request $request)
     {
-        dd($request->all());
+        // dd($request->all());
         $validator = Validator($request->all(), [
             'name_ar' => 'required|string|min:3',
             'name_en' => 'required|string|min:3',
