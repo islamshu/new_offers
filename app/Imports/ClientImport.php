8f35da2391959c1c18f6dc4dee99f493864fe31a
islamshu12 <islamshu12@gmail.com>
2022-01-30 20:39:45 +0200

    d

--- app/Imports/ClientImport.php
+++ app/Imports/ClientImport.php
@@ -21,6 +21,7 @@ class ClientImport implements ToCollection
             if($row[0]  == null || $row[1] == null || $key == 0 || $key == 1 ){
                 continue;
             }
+            dd($row);
           
        
             $client = new Clinet();
