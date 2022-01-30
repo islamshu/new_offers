68e4060fd764426c3cc728aa00506724481bea1c
islamshu12 <islamshu12@gmail.com>
2022-01-30 20:47:44 +0200

    s

--- app/Imports/ClientImport.php
+++ app/Imports/ClientImport.php
@@ -21,7 +21,7 @@ class ClientImport implements ToCollection
             if($row[0]  == null || $row[1] == null || $key == 0 || $key == 1 ){
                 continue;
             }
-            dd($row[8]);
+         
           
        
             $client = new Clinet();
@@ -33,7 +33,7 @@ class ClientImport implements ToCollection
             $client->register_time =  $row[5];
             $client->type_of_subscribe = $row[6];
             $client->number_of_operations =  $row[7];
-            $client->last_transaction =   Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[8]));
+            $client->last_transaction =   $row[8];
             $client->register_date =   Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[9]));
             $client->mobile_type =  $row[10];
             $client->expire_date =   Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[11]));
