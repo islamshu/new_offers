1fa596152993877e2bbb36de7c840bfbb5f91e2d
islamshu12 <islamshu12@gmail.com>
2022-01-30 20:36:22 +0200

    x

--- app/Imports/ClientImport.php
+++ app/Imports/ClientImport.php
@@ -21,22 +21,21 @@ class ClientImport implements ToCollection
             if($row[0]  == null || $row[1] == null || $key == 0 || $key == 1 ){
                 continue;
             }
-$ss=            Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[9]));
-            dd($ss);
+          
+       
             $client = new Clinet();
             $client->name = $row[0];
             $client->phone = $row[1];
             $client->email =  $row[2];
-            $client->birth_date = is_date($row[3]);
+            $client->birth_date = is_date( Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[3])));
             $client->nationality =  $row[4];
             $client->register_time =  $row[5];
             $client->type_of_subscribe = $row[6];
             $client->number_of_operations =  $row[7];
             $client->last_transaction =  $row[8];
-            $client->register_date =  $row[9];
+            $client->register_date =   Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[9]));
             $client->mobile_type =  $row[10];
-            $client->expire_date =  $row[11];
-
+            $client->expire_date =   Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row[11]));
             
             $client->save();
 
