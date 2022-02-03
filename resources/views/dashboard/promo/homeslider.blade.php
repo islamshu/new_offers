cc3a08f272c8126223a9f06b2d779f2a7a6d2158
islamshu12 <islamshu12@gmail.com>
2022-02-03 17:32:56 +0200

    f

--- resources/views/dashboard/promo/homeslider.blade.php
+++ resources/views/dashboard/promo/homeslider.blade.php
@@ -29,7 +29,8 @@
 
                     <td>{{ $item->title_ar }}</td>
                     <td>{{ $item->title_en }}</td>
-                    <td ><button style="background: {{ $item->color  }}">{{ $item->color }}</button></td>  
+                    <td><input type="color" disabled value="{{ $item->color }}"></td>
+                    {{-- <td ><button style="background: {{ $item->color  }}">{{ $item->color }}</button></td>   --}}
                     <td>{{ $item->city->city_name_english }}</td>
                     <td>{{ $item->sort }}</td>
 
