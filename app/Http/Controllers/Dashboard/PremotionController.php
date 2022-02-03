9443a01bddba913ad4fec5650700a2aee0c25060
islamshu12 <islamshu12@gmail.com>
2022-02-03 12:54:44 +0200

    d

--- app/Http/Controllers/Dashboard/PremotionController.php
+++ app/Http/Controllers/Dashboard/PremotionController.php
@@ -68,8 +68,8 @@ class PremotionController extends Controller
 
     }
     public function homeslider_delete($locale ,$id){
-        $premotions = Homeslider::where('id',$id)->get();
-        dd($locale,$id);
+        $premotions = HomesliderOffer::where('id',$id)->get();
+        dd($premotions);
         return response()->json(['icon' => 'success', 'title' => 'homeslider deleted successfully'], 200);
 
     }
