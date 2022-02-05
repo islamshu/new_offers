facefa0981c385ee7d02043b7c078ee30dfe9050
islamshu12 <islamshu12@gmail.com>
2022-02-05 12:39:03 +0200

    d

--- app/Http/Resources/VendorOfferCollection.php
+++ app/Http/Resources/VendorOfferCollection.php
@@ -18,7 +18,7 @@ class VendorOfferCollection extends ResourceCollection
             'count' => $this->count(),
             'total' => $this->total(),
             'prev'  => $this->previousPageUrl(),
-            'next'  => $this->nextPageUrl(), 
+            'next'  => $this->appends(request()->input())->nextPageUrl(), 
             'offers' =>VendorOfferResourses::collection($this->collection),
         ];
     }
