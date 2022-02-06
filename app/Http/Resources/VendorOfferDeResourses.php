3c5d672e9174d9aa641f1944a3200ba85315d870
islamshu12 <islamshu12@gmail.com>
2022-02-06 19:42:44 +0200

    f

--- app/Http/Resources/VendorOfferDeResourses.php
+++ app/Http/Resources/VendorOfferDeResourses.php
@@ -19,33 +19,44 @@ class VendorOfferDeResourses extends JsonResource
         return[
             'id'=>$this->id,
             
-            // 'name'=>$this->lang_name($this),
-            // 'details'=>$this->lang_details($this),
-            // 'terms'=>$this->lang_terms($this),
-            // 'image'=>asset('images/primary_offer/'.@$this->offerimage->primary_image),
-            // 'type'=>$this->offer_type,
-            // 'membership_type'=>$this->member_type,
-            // 'start_date'=>$this->start_time,
-            // 'expire_date'=>$this->end_time,
-            // 'before_price'=>'',
-            // 'price'=> 0,
-            // 'client_usage_times'=> $this->usege_member,
-            // 'total_usage_times'=> $this->usege_system,
-            // 'limit_period_duration'=>null,
-            // 'limit_period_duration'=>null,
-            // 'limit_period_unit'=>null,
-            // 'points_no'=>$this->system_point,
-            // 'store_points_no'=>$this->store_point,
-            // 'is_general'=>0,
-            // 'estimated_saving'=>0,
-            // 'is_favorite'=>$this->is_fav($this),
-            // 'percentage'=> null,
-            // 'flash_deal'=> $this->is_flashdeal,
-            // 'voucher'=> $this->is_voucher,
-            // 'store_id'=> $this->vendor_id ,
-            // 'distance'=>@$this->get_dinstance($this,$request),
-            // 'store'=> new VendorForOfferResourses($this->vendor)  ,
+            'name'=>$this->lang_name($this),
+            'details'=>$this->lang_details($this),
+            'terms'=>$this->lang_terms($this),
+            'image'=>asset('images/primary_offer/'.@$this->offerimage->primary_image),
+            'type'=>$this->typeoffer($this),
+            'membership_type'=>strtoupper($this->member_type),
+            'start_date'=>$this->start_time,
+            'expire_date'=>$this->end_time,
+            'before_price'=>'',
+            'price'=> 0,
+            'before_price'=>$this->offertype->price_befor_discount,
+            'price'=> $this->offertype->price != null ? $this->offertype->price : 0 ,
+            'percentage'=>$this->offertype->discount_value != null ? $this->offertype->discount_value : 0,
+            'client_usage_times'=>$this->usege_member  == 'unlimit' ? null : (int)$this->usage_member_number ,
+            'total_usage_times'=> $this->check($this),
+            'limit_period_duration'=>null,
+            'limit_period_duration'=>null,
+            'limit_period_unit'=>null,
+            'points_no'=>$this->system_point,
+            'store_points_no'=>$this->store_point,
+            'is_general'=>0,
+            'estimated_saving'=>0,
+            'is_favorite'=>$this->is_fav($this),
+            'percentage'=> null,
+            'flash_deal'=> $this->is_flashdeal,
+            'voucher'=> $this->is_voucher,
+            'store_id'=> $this->vendor_id ,
+            'distance'=>@$this->get_dinstance($this,$request),
+            'store'=> new VendorForOfferResourses($this->vendor)  ,
         ];
+    }
+    public function check($data){
+        if (auth('client_api')->check()) {
+      $trans = Transaction::where('offer_id',$data->id)->where('client_id',auth('client_api')->id())->count();
+      return $trans;
+        }else{
+            return null;
+        }
     }
     public function get_dinstance($data,$request)
     {
@@ -97,15 +108,42 @@ class VendorOfferDeResourses extends JsonResource
     }
     public function lang_terms($data)
     {
+        
+        $array = [];
         $lang = request()->header('Lang');
         if ($lang != null) {
             if ($lang  == 'ar') {
-                return $data->terms_ar;
+             
+               $items = explode('-',$data->terms_ar);
+            foreach($items as $key=>$i){
+               array_push($array,$items[$key]);
+             }
+               return $array;
+
             } else {
-                return $data->terms_en;
+               
+                $items = explode('-',$data->terms_en);
+             foreach($items as $key=>$i){
+                array_push($array,$items[$key]);
+              }
+                return $array;
             }
         } else {
-            return $data->terms_en;
+          
+            $items = explode('-',$data->terms_en);
+         foreach($items as $key=>$i){
+            array_push($array,$items[$key]);
+          }
+            return $array;
+        }
+    }
+    public function typeoffer($data){
+        if($data->offertype->offer_type =='buyOneGetOne'){
+            return 'buy_1_get_1';
+        }elseif($data->offertype->offer_type =='special_discount'){
+            return 'special_discount';
+        }elseif($data->offertype->offer_type =='general_offer'){
+            return 'general_discount';
         }
     }
 }
