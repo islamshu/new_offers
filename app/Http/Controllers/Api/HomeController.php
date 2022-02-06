eaf5f00651d286352a909ccc2417fe52188e22e4
islamshu12 <islamshu12@gmail.com>
2022-02-06 20:26:03 +0200

    d

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -303,14 +303,21 @@ class HomeController extends BaseController
       }
     }
     
+
+   if($data_show){
     $res['status'] = $this->sendResponse200('OK');
     $array = [];
-    dd($data_show);
+  
     array_push($array, new PopupResoures($data_show));
     $res['data']['popup_ads'] = $array;
      
-    return $res;
+ 
+  }else{
+    $res['status']=$this->sendError();
+    $res['meessage'] = 'not found popup';
+  
   }
+  return  $res;
 
-
+}
 }
