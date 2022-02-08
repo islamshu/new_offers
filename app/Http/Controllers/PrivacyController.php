cea343547f8c22d5c4e027d16bbdfc82b4f1fd05
islamshu12 <islamshu12@gmail.com>
2022-02-08 14:09:29 +0200

    f

---
+++ app/Http/Controllers/PrivacyController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Privacy;
+use Illuminate\Http\Request;
+
+class PrivacyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('dashboard.pages.privacy');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $page = new Privacy();
+        $page->title_ar = $request->title_ar;
+        $page->title_en = $request->title_en;
+        $page->content_ar = $request->content_ar;
+        $page->content_en = $request->content_en;
+        $page->sort = $request->sort;
+        $page->save();
+        return redirect()->back()->with(['succss'=>trans('add succeefully')]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Privacy  $privacy
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Privacy $privacy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Privacy  $privacy
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Privacy $privacy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Privacy  $privacy
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Privacy $privacy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Privacy  $privacy
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Privacy $privacy)
+    {
+        //
+    }
+}
---
+++ resources/views/dashboard/pages/privacy.blade.php
@@ -0,0 +1,60 @@
+@extends('layout.default')
+
+@section('content')
+<div class="card card-custom">
+
+    <div class="card-header">
+        <h3 class="card-title">
+            Create Privacy
+        </h3>
+        <div class="card-toolbar">
+            <div class="example-tools justify-content-center">
+                <span class="example-toggle" data-toggle="tooltip" title="View code"></span>
+                <span class="example-copy" data-toggle="tooltip" title="Copy code"></span>
+            </div>
+        </div>
+    </div>
+    <form class="form" method="post" method="{{ route('privacy.store') }}" enctype="multipart/form-data">
+        @csrf
+        <div class="card-body">
+            <div class="row">
+                {{-- <div class="form-group col-md-12">
+                    <select class="form-control select2 vendor" id="kt_select2_1" name="param">
+                        <option value='null'>{{ __('chose Vendor') }}</option>
+                        @foreach ($vendor as $item)
+                        <option value={{$item->id}}>{{$item->name_en}}</option>
+                        @endforeach
+                    </select>
+                </div> --}}
+                <div class="form-group col-md-6">
+                    <label> {{ __('title ar') }} :</label>
+                    <input type="text" name="title_ar" id="name_ar" class="form-control form-control-solid"
+                        placeholder="Enter Title" required />
+                </div>
+                <div class="form-group col-md-6">
+                    <label>{{ __('title en') }} :</label>
+                    <input type="text" name="title_en" id="name_en" class="form-control form-control-solid"
+                        placeholder="Enter Title" required />
+                </div>
+                <div class="form-group col-md-6">
+                    <label>{{ __('Content ar') }} :</label>
+                    <textarea name="content_ar" class="form-control" required id="" cols="30" rows="10"></textarea>
+                </div>
+                <div class="form-group col-md-6">
+                    <label>{{ __('Content en') }} :</label>
+                    <textarea name="content_en" class="form-control" required id="" cols="30" rows="10"></textarea>
+                </div>
+                <div class="form-group col-md-6">
+                    <label>{{ __('sort') }} :</label>
+                    <input type="number" name="sort" id="sort" class="form-control form-control-solid"
+                        placeholder="Enter sort" required />
+                </div>
+
+
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary mr-2">{{ __('Submit') }}</button>
+            </div>
+    </form>
+</div>
+</div>
+@endsection
