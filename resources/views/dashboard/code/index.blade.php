b25c358d73c654f37bb3dcf03c2752108da7fe61
islamshu12 <islamshu12@gmail.com>
2022-02-10 11:53:08 +0200

    d

--- resources/views/dashboard/code/index.blade.php
+++ resources/views/dashboard/code/index.blade.php
@@ -22,7 +22,10 @@
                     @foreach ($codes as $code)
                         <td>{{ $code->name_en }}</td>
                         <td>{{ $code->number_of_code }}</td>
-                        <td>{{ $code->total_remain }}</td>
+                        @php
+                            $remain = App\Models\CodeSubscription::where('sub_id',$code->sub_id)->where('is_used',0)->count()
+                        @endphp
+                        <td>{{ $remain }}</td>
                         <td>
                             <input type="checkbox" data-id="{{ $code->id }}" name="status" class="js-switch" {{ $code->status ==  1 ? 'checked' : '' }}>
                             </td>
