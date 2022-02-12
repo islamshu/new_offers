ce31ab70042722b2d13a1b51224bb955ae7415f2
islamshu12 <islamshu12@gmail.com>
2022-02-12 11:57:21 +0200

    c

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -114,6 +114,7 @@ class UserController extends BaseController
 
             // $res['token'] = $user->createToken('Personal Access Token')->token;
             // $res['data'][""]="";
+            $res['data']['client']['token']=$user->createToken('Personal Access Token')->accessToken;
             $res['other']['is_trial_subscriber']= false;
             return $res;
         }else{
