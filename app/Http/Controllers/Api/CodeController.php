272e2ab4a28483cb41f2457cc5fb5febdc3d1319
islamshu12 <islamshu12@gmail.com>
2022-02-12 12:58:33 +0200

    5

--- app/Http/Controllers/Api/CodeController.php
+++ app/Http/Controllers/Api/CodeController.php
@@ -49,11 +49,18 @@ class CodeController extends BaseController
         }elseif($code->type_balance == 'UnLimit'){
             $client->is_limited = 1;
         }
-        $client->start_date = $code->start_date;
-        $client->expire_date = $code->end_date;
-        $client->save();
+        $client->start_date = Carbon::now();
         $data_type = $code->expire_date_type;
         $data_type_number = $code->number_of_dayes;
+        if($data_type == 'days'){
+            $$client->expire_date = Carbon::now()->addDays($data_type_number);
+        }elseif($data_type == 'months'){
+            $$client->expire_date = Carbon::now()->addMonths($data_type_number);
+        }elseif($data_type == 'years'){
+            $$client->expire_date = Carbon::now()->addYears($data_type_number);
+        }
+        $client->save();
+       
         if($data_type == 'days'){
             $user->expire_date = Carbon::now()->addDays($data_type_number);
         }elseif($data_type == 'months'){
