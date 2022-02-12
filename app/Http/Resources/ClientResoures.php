f525edd3acfc1ad7c58d187900e1df9602f356f3
islamshu12 <islamshu12@gmail.com>
2022-02-12 12:02:27 +0200

    f

--- app/Http/Resources/ClientResoures.php
+++ app/Http/Resources/ClientResoures.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\City;
 use App\Models\Country;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -16,28 +17,28 @@ class ClientResoures extends JsonResource
     public function toArray($request)
     {
         return [
-            'name'=>$this->username,
+            'name'=>$this->name,
             'email'=>$this->email,
             'phone'=>(int)$this->phone,
-            'gender'=>(int)$this->gender,
-            'birth_date'=>(string)$this->birth_date,
+            'gender'=>$this->gender,
+            'birth_date'=>$this->birth_date == null  ? null :date('Y-m-d', strtotime((string)$this->birth_date)),
             'image'=>asset($this->image),
             'nationality'=>$this->nationality,
             'is_complete'=>(int)$this->is_complete,
             'is_subscriber'=>(int)$this->is_subscriber,
             'is_new_version'=>1,
             'is_subscriber_in_city'=>0,
-            'is_unlimited'=>(int)$this->is_unlimited,
+            'is_unlimited'=>$this->is_unlimited,
             'is_employee'=>(int)$this->is_unlimited,
-            'is_family'=>(int)$this->is_family,
+            'is_family'=>$this->is_family,
             'multiple_accounts_no'=>$this->multiple_accounts_no,
-                        'actual_accounts_no'=>$this->actual_accounts_no == null ? 0 : $this->actual_accounts_no  
- ,
-            'status'=>$this->status == 'active' ? 1 : 0 ,
+            'actual_accounts_no'=>$this->actual_accounts_no == null ? 0 : $this->actual_accounts_no  
+ ,           // 'status'=>$this->status == 'active' ? 1 : 0 ,
             'country'=>new CountryResoures(Country::find($this->country_id)),
-            'city' => @$this->city->neglish_name,
+            'city' => new CityResoures(City::find($this->city_id)),
+        
             'subscription'=>new SubResoures(@$this),
-            'token' =>$this->createToken('Personal Access Token')->accessToken,
+            'token'=>$this->createToken('Personal Access Token')->accessToken
         ];
     }
 }
