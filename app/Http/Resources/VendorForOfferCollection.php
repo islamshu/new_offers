4cfcad3d038d67b96c38e10d48da7e278dab1dfe
islamshu12 <islamshu12@gmail.com>
2022-02-15 16:05:02 +0200

    s

--- app/Http/Resources/VendorForOfferCollection.php
+++ app/Http/Resources/VendorForOfferCollection.php
@@ -25,7 +25,7 @@ class VendorForOfferCollection extends ResourceCollection
         $sliders = Slider::where('categoty_id',$request->category_id)->where('country_id',$request->country_id)->where('city_id',$request->city_id)->get();
         $TEST = paginate($datad,$limit,$page);
         return [
-            'stores' =>[$TEST],
+            'stores' =>$TEST,
             'category_slider_images' => new SliderCollection($sliders),
         ];
     }
