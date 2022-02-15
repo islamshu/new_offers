a8d2e3919d78d954b5722ea456e27a2d11b64f15
islamshu12 <islamshu12@gmail.com>
2022-02-15 14:54:02 +0200

    d

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Models\Enterprise;
+use Illuminate\Container\Container;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
 
 function openJSONFile($code){
     $jsonString = [];
@@ -85,4 +89,33 @@ function offer_type($type){
     }elseif($type =='special_discount'){
         return 'Special Discount';
     }
+}
+ function paginate(Collection $results, $pageSize)
+{
+    $page = Paginator::resolveCurrentPage('page');
+    
+    $total = $results->count();
+
+    $this->paginator($results->forPage($page, $pageSize), $total, $pageSize, $page, [
+        'path' => Paginator::resolveCurrentPath(),
+        'pageName' => 'page',
+    ]);
+
+}
+
+/**
+ * Create a new length-aware paginator instance.
+ *
+ * @param  \Illuminate\Support\Collection  $items
+ * @param  int  $total
+ * @param  int  $perPage
+ * @param  int  $currentPage
+ * @param  array  $options
+ * @return \Illuminate\Pagination\LengthAwarePaginator
+ */
+ function paginator($items, $total, $perPage, $currentPage, $options)
+{
+    return Container::getInstance()->makeWith(LengthAwarePaginator::class, compact(
+        'items', 'total', 'perPage', 'currentPage', 'options'
+    ));
 }
