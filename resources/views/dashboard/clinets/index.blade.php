d3cb791cd8ad91e2fa68c8cfbab8149e9ce22eb7
islamshu12 <islamshu12@gmail.com>
2022-02-15 11:59:28 +0200

    c

--- resources/views/dashboard/clinets/index.blade.php
+++ resources/views/dashboard/clinets/index.blade.php
@@ -99,7 +99,7 @@
                                             onclick="make('{{ $item->id }}')">Send Notofication</a>
 
                                         <a data-toggle="modal" data-target="#addSub"
-                                            onclick="makenew_fun('{{ $item->id }}')">Add Subscribe</a>
+                                            onclick="makenew_fun({{ $item->id }})">Add Subscribe</a>
 
                                     </div>
 
