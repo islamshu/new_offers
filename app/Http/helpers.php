c9c992db9e25f4b4d85a9e8bb1529e1a85f347a9
islamshu12 <islamshu12@gmail.com>
2022-02-15 15:07:34 +0200

    d

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -90,7 +90,7 @@ function offer_type($type){
         return 'Special Discount';
     }
 }
-function paginate($items, $perPage = 5, $page = null, $options = [])
+function paginate($items, $limit = 5, $page = null, $options = [])
 {
     $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
     $items = $items instanceof Collection ? $items : Collection::make($items);
