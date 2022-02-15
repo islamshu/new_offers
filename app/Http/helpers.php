e2a57b22dd114c391215161caee534486a7126b7
islamshu12 <islamshu12@gmail.com>
2022-02-15 15:49:03 +0200

    s

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -94,5 +94,5 @@ function paginate($items, $limit = 5, $page = null, $options = [])
 {
     $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
     $items = $items instanceof Collection ? $items : Collection::make($items);
-    return $items->forPage($page, $limit);
+    return $items;
 }
