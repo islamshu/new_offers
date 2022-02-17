d912b27082fb8f1c5ddfd54e9109c124a77fe04e
islamshu12 <islamshu12@gmail.com>
2022-02-17 15:38:20 +0200

    ب

--- resources/views/dashboard/generalinfo/index.blade.php
+++ resources/views/dashboard/generalinfo/index.blade.php
@@ -28,17 +28,17 @@
 
                 <div class="form-group col-md-6">
                     <label>{{ __('Sender Id sms') }}:</label>
-                    <input type="text" name="sender_id" id="sender_id" class="form-control form-control-solid"
+                    <input type="text" name="general[sender_id]" id="sender_id" class="form-control form-control-solid"
                         placeholder="Enter Sender Id"  />
                 </div>
                 <div class="form-group col-md-6">
                     <label>{{ __('Token for sms') }}:</label>
-                    <input type="text" name="sms_token" id="sms_token" class="form-control form-control-solid"
+                    <input type="text" name="general[sms_token]" id="sms_token" class="form-control form-control-solid"
                         placeholder="Token for sms"  />
                 </div>
                 <div class="form-group col-md-6">
                     <label>{{ __('password for sms') }}:</label>
-                    <input type="text" name="sms_password" id="sms_password" class="form-control form-control-solid"
+                    <input type="text" name="general[sms_password]" id="sms_password" class="form-control form-control-solid"
                         placeholder="Password for sms" required />
                 </div>
              
