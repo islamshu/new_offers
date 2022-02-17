c855255109da957fc7caa338e89d68bd3ca40d38
islamshu12 <islamshu12@gmail.com>
2022-02-17 15:56:06 +0200

    d

--- resources/views/dashboard/generalinfo/index.blade.php
+++ resources/views/dashboard/generalinfo/index.blade.php
@@ -27,9 +27,9 @@
             <div class="row">
                 <div class="form-group col-md-6">
                     <label>{{ __('Active in Live') }}:</label>
-                    <select name="general[actvie_sms]" value="(int){{ get_general('actvie_sms') }}" class="form-control form-control-solid" id="">
-                        <option value="0">no</option>
-                        <option value="1">yes</option>
+                    <select name="general[actvie_sms]" value="{{ get_general('actvie_sms') }}" class="form-control form-control-solid" id="">
+                        <option value="0" @if(get_general('actvie_sms')  == '0') selected @endif>no</option>
+                        <option value="1" @if(get_general('actvie_sms')  == '1') selected @endif>yes</option>
 
                     </select>
                    
