559161ca6f7f34a8f2d17971cfdd451c8cdd9f80
islamshu12 <islamshu12@gmail.com>
2022-02-17 15:36:15 +0200

    ي

--- resources/views/dashboard/generalinfo/index.blade.php
+++ resources/views/dashboard/generalinfo/index.blade.php
@@ -27,9 +27,19 @@
             <div class="row">
 
                 <div class="form-group col-md-6">
-                    <label>{{ __('Name ar') }}:</label>
-                    <input type="text" name="name_ar" id="name_ar" class="form-control form-control-solid"
-                        placeholder="Enter Name" required />
+                    <label>{{ __('Sender Id sms') }}:</label>
+                    <input type="text" name="sender_id" id="sender_id" class="form-control form-control-solid"
+                        placeholder="Enter Sender Id"  />
+                </div>
+                <div class="form-group col-md-6">
+                    <label>{{ __('Token for sms') }}:</label>
+                    <input type="text" name="sms_token" id="sms_token" class="form-control form-control-solid"
+                        placeholder="Token for sms"  />
+                </div>
+                <div class="form-group col-md-6">
+                    <label>{{ __('password for sms') }}:</label>
+                    <input type="text" name="sms_password" id="sms_password" class="form-control form-control-solid"
+                        placeholder="Password for sms" required />
                 </div>
              
                
