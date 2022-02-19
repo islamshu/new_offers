8a7dce70d46ee6fb86b2380525038987bf518864
islamshu12 <islamshu12@gmail.com>
2022-02-19 16:05:35 +0200

    s

--- app/Http/Controllers/GeneralInfoController.php
+++ app/Http/Controllers/GeneralInfoController.php
@@ -18,10 +18,10 @@ class GeneralInfoController extends Controller
     public function index()
     {
         $token = get_general('sms_token');
-       $url= ('https://api.oursms.com/api-a/billing/credits?token=whyfA4pML1nN4w3Yj7_WpKDo29NIOWav-0EqK38KRco');
-        $json = json_decode(file_get_contents($url), true);
+       $url= 'https://api.oursms.com/api-a/billing/credits?token=whyfA4pML1nN4w3Yj7_WpKDo29NIOWav-0EqK38KRco';
+       return_redirect($url);
 
-        dd($json);
+       
         return view('dashboard.generalinfo.index');
     }
      
--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -121,7 +121,14 @@ function send_message($phone,$message)
    
 }
 function return_redirect($url){
-   return Http::get($url);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+    $html = curl_exec($ch);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
 
 }
 function get_general($key)
