81fdb64fa5d07831f043f8cb1636546ddd4ad6b2
islamshu12 <islamshu12@gmail.com>
2022-02-19 15:42:35 +0200

    d

--- app/Http/Controllers/GeneralInfoController.php
+++ app/Http/Controllers/GeneralInfoController.php
@@ -18,8 +18,7 @@ class GeneralInfoController extends Controller
     public function index()
     {
         $token = get_general('sms_token');
-      $url = 'https://api.oursms.com/api-a/billing/credits?token=whyfA4pML1nN4w3Yj7_WpKDo29NIOWav-0EqK38KRco';
-
+        return 'https://api.oursms.com/api-a/billing/credits?token=whyfA4pML1nN4w3Yj7_WpKDo29NIOWav-0EqK38KRco';
    
     $response = Http::acceptJson()->get($url);
     dd($response);
