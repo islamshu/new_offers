3801ba4783ff6b1e716f8210b121f4bb49426e3a
islamshu12 <islamshu12@gmail.com>
2022-02-19 15:34:18 +0200

    ب

--- app/Http/Controllers/GeneralInfoController.php
+++ app/Http/Controllers/GeneralInfoController.php
@@ -21,7 +21,7 @@ class GeneralInfoController extends Controller
       $url = 'https://api.oursms.com/api-a/billing/credits?token=whyfA4pML1nN4w3Yj7_WpKDo29NIOWav-0EqK38KRco';
 
    
-    $response = Http::acceptJson()->get('application/json')->get($url);
+    $response = Http::acceptJson()->get($url);
     dd($response);
         return view('dashboard.generalinfo.index');
     }
