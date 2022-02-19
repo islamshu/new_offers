1eef62e7af7ae28f4f977fa60a6a408cc6a49277
islamshu12 <islamshu12@gmail.com>
2022-02-19 14:38:03 +0200

    f

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -105,6 +105,7 @@ function paginate($items, $limit, $page , $options = [])
 }
 function send_message($phone,$message)
 {
+    dd('dd');
     $token = get_general('sms_token');
     $url = 'https://api.oursms.com/api-a/msgs?token='.$token.'&src=string&dests='.$phone.'&body'.$message;
     $response = Http::get($url);
