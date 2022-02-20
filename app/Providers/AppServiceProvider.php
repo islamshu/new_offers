773825912a13f2a107df3c6b339eb0c760727094
islamshu12 <islamshu12@gmail.com>
2022-02-20 13:46:32 +0200

    f

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -39,9 +39,7 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrap();
 
         Schema::defaultStringLength(191);
-        if (config('app.env') === 'production') {
-            URL::forceSchema('https');
-        }
+      
 
     }
 }
