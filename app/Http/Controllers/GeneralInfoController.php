733a51508ca52d68e34ea234184c413dc2dca3fd
islamshu12 <islamshu12@gmail.com>
2022-02-20 12:57:43 +0200

    ي

--- app/Http/Controllers/GeneralInfoController.php
+++ app/Http/Controllers/GeneralInfoController.php
@@ -26,10 +26,9 @@ class GeneralInfoController extends Controller
     }
     public function test()
     {
-        $body =  Http::get('https://api.oursms.com/api-a/billing/credits?token=whyfA4pML1nN4w3Yj7_WpKDo29NIOWav-0EqK38KRco');
+        $response = Http::withoutVerifying()->get('http://jsonplaceholder.typicode.com/todos/2');
 
-
-        dd($body);
+            dd($response->json());
     }
 
 
