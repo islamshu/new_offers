566670a9f9382d7a7ae045c910dae11638a81b6d
islamshu12 <islamshu12@gmail.com>
2022-02-21 17:07:03 +0200

    s

--- app/Http/Controllers/Api/PayemntController.php
+++ app/Http/Controllers/Api/PayemntController.php
@@ -65,7 +65,7 @@ class PayemntController extends BaseController
             return $res;
         }
         $json = json_decode($response);
-        dd($json->Data);
+        dd($json->Data->InvoiceId);
         if (isset($json->IsSuccess) && $json->IsSuccess == true) {
             $count = Subscriptions_User::where('clinet_id', auth('client_api')->id())->where('sub_id', $code->id)->count();
             $user = new Subscriptions_User();
