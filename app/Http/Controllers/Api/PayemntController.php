7738aa6b2e5dc2248f6670d4eb03404b83750558
islamshu12 <islamshu12@gmail.com>
2022-02-21 16:32:24 +0200

    h

--- app/Http/Controllers/Api/PayemntController.php
+++ app/Http/Controllers/Api/PayemntController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Api\BaseController;
 use App\Models\DiscountSubscription;
 use App\Models\Subscription;
+use App\Models\Subscriptions_User;
 use Carbon\Carbon;
 use Facade\FlareClient\Time\Time;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx\Rels;
@@ -14,25 +15,67 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx\Rels;
 class PayemntController extends BaseController
 {
 public function myfatoorah(Request $request){
-    dd(Carbon::now()->timestamp);
-    $pakege = Subscription::find($request->package_id);
-    if(!$pakege){
+    
+    $code = Subscription::find($request->package_id);
+    if(!$code){
         $res['status'] = $this->SendError();
         $res['status']['message'] = 'Pakege Not Found';
         return $res;
     }
-    $price = $pakege->price;
-    if($request->promo_code != null){
-     $promo=   DiscountSubscription::where('code',$request->promo_code)->first();
-     if($promo){
+    $price = $code->price;
+//     $count = Subscriptions_User::where('clinet_id', auth('client_api')->id())->where('sub_id', $code->id)->count();
+//     $user = new Subscriptions_User();
+//         $user->payment_type = 'activition_code';
+//         // dd(auth('client_api')->id());
+//         $client = auth('client_api')->user();
+//         $client->type_of_subscribe = $code->type_paid;
 
-     }
-    }
+//         if ($code->type_balance == 'Limit') {
+//             $client->is_unlimited = 0;
+//             $client->credit = $code->balance;
+//             $client->remain = $code->balance;
+//         } elseif ($code->type_balance == 'UnLimit') {
+//             $client->is_unlimited = 1;
+//             $client->credit = null;
+//             $client->remain = null;
+//         }
+//         $client->start_date = Carbon::now();
+//         $data_type = $code->expire_date_type;
+//         $data_type_number = $code->number_of_dayes;
+//         if ($data_type == 'days') {
+//             $client->expire_date = Carbon::now()->addDays($data_type_number);
+//         } elseif ($data_type == 'months') {
+//             $client->expire_date = Carbon::now()->addMonths($data_type_number);
+//         } elseif ($data_type == 'years') {
+//             $client->expire_date = Carbon::now()->addYears($data_type_number);
+//         }
+//         $client->save();
+
+//         if ($data_type == 'days') {
+//             $user->expire_date = Carbon::now()->addDays($data_type_number);
+//         } elseif ($data_type == 'months') {
+//             $user->expire_date = Carbon::now()->addMonths($data_type_number);
+//         } elseif ($data_type == 'years') {
+//             $user->expire_date = Carbon::now()->addYears($data_type_number);
+//         }
+//         $user->status = 'active';
+//         $user->balnce = $code->balance;
+//         $user->purchases_no =  $count + 1;
+//         $user->sub_id  = $code->id;
+//         $user->clinet_id  = auth('client_api')->id();
+//         $user->save();
+
+//     // if($request->promo_code != null){
+//     //  $promo=   DiscountSubscription::where('code',$request->promo_code)->first();
+//     //  if($promo){
+
+//     //  }
+//     // }
     
     $postFields = [
         //Fill required data
         'NotificationOption' => 'Lnk', //'SMS', 'EML', or 'ALL'
-        'InvoiceValue'       => '50',
+        'InvoiceValue'       => $price,
         'CustomerName'       => $request->customer_name,
             //Fill optional data
             'DisplayCurrencyIso' => $request->currency_iso_code != null ? $request->currency_iso_code : 'SAR',
