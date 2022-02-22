72caabef5756eba7ffee4335cc34dd4c3653e758
islamshu12 <islamshu12@gmail.com>
2022-02-22 11:44:15 +0200

    d

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -239,6 +239,8 @@ class HomeController extends BaseController
     $res['status'] = $this->sendResponse('OK');
     $res['data']['myfatoorah_credentials']['api_key']=get_general('api_key') ;
     $res['data']['myfatoorah_credentials']['base_url']=get_general('base_url') ;
+    return response()->setStatusCode()->json;
+
     return $res;
 
   }
