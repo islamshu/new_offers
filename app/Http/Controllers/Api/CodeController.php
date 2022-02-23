f3ba12a5e3b95dc09cc4c25f03d1558996468f90
islamshu12 <islamshu12@gmail.com>
2022-02-23 13:29:54 +0200

    d

--- app/Http/Controllers/Api/CodeController.php
+++ app/Http/Controllers/Api/CodeController.php
@@ -147,8 +147,8 @@ class CodeController extends BaseController
                 $ofe->branch_id = $request->branch_id;
                 $user->purchases_no += 1;
                 if ($type_of_offer != 'free') {
-                    dd($user->remain );
-                    if ($user->remain > 0) {
+                   
+                    if ($user->remain > 0 && $user->remain != null) {
                         $user->remain = $user->remain - 1;
                     } else {
                         $res['status'] = $this->SendError();
