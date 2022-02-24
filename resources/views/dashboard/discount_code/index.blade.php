a3471218516b240d9f65dd596c2abfa2ddd8af9e
islamshu12 <islamshu12@gmail.com>
2022-02-24 10:57:06 +0200

    d

--- resources/views/dashboard/discount_code/index.blade.php
+++ resources/views/dashboard/discount_code/index.blade.php
@@ -28,7 +28,8 @@
                             </td>
                         <td class="pr-0 text-left">
 
-
+                            <a data-toggle="modal" data-target="#myModal" class="btn btn-outline-primary"
+                            onclick="make('{{ $item->id }}')"><i class="fa fa-eye"></i></a>
                                 <a href="{{ route('discount_code.edit', [ $code->id, 'locale' => app()->getLocale()]) }}"
                                     class="btn btn-icon btn-light btn-hover-primary btn-sm mx-3">
                                     <span class="svg-icon svg-icon-md svg-icon-primary">
