ec59024f935134e61b2f8cac5dd74ac81ddaf3c0
islamshu12 <islamshu12@gmail.com>
2022-03-01 12:03:02 +0200

    d

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,4 +7,13 @@ use Laratrust\Models\LaratrustRole;
 class Role extends LaratrustRole
 {
     public $guarded = [];
+    /**
+     * Get all of the comments for the Role
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function permissions()
+    {
+        return $this->hasMany(Permission::class, 'role_id');
+    }
 }
