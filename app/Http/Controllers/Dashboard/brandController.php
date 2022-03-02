368edfc4ff44771215b08c88415e89da17675c1f
islamshu12 <islamshu12@gmail.com>
2022-03-02 15:23:02 +0200

    s

--- app/Http/Controllers/Dashboard/brandController.php
+++ app/Http/Controllers/Dashboard/brandController.php
@@ -47,7 +47,6 @@ class brandController extends Controller
     }//end of constructor
     public function index()
     {
-        dd(auth()->user()->permissions);
         dd(auth()->user()->hasPermission('update-vendor'),auth()->user()->hasPermission('read-vendor'),auth()->user()->hasPermission('create-vendor'),auth()->user()->hasPermission('delete-vendor'));
 
         if (Auth::user()->hasRole('Admin')) {
