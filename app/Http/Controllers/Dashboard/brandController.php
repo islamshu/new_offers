70129cdb30a96986df902ca986f11929e577d64b
islamshu12 <islamshu12@gmail.com>
2022-03-02 14:34:29 +0200

    d

--- app/Http/Controllers/Dashboard/brandController.php
+++ app/Http/Controllers/Dashboard/brandController.php
@@ -39,6 +39,7 @@ class brandController extends Controller
     public function __construct()
     {
         //create read update delete
+        dd(auth()->user()->allPermissions());
         $this->middleware(['permission:read-vender'])->only('index');
         // $this->middleware(['permission:create_users'])->only('create');
         // $this->middleware(['permission:update_users'])->only('edit');
