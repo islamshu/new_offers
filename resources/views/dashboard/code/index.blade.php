01f8efe3446e4536175fbb6df0f2592904026875
islamshu12 <islamshu12@gmail.com>
2022-03-03 13:14:13 +0200

    f

--- resources/views/dashboard/code/index.blade.php
+++ resources/views/dashboard/code/index.blade.php
@@ -47,13 +47,15 @@
                             <input type="checkbox" data-id="{{ $code->id }}" name="status" class="js-switch" {{ $code->status ==  1 ? 'checked' : '' }}>
                             </td>
                         <td class="pr-0 text-left">
+                            @if (auth()->user()->isAbleTo(['update-promocode']))
 
 
                                 <a href="{{ route('code.edit', [ $code->id, 'locale' => app()->getLocale()]) }}"
                                     class="btn btn-icon btn-light btn-hover-primary btn-sm mx-3">
                                  <i class="fa fa-edit"></i>
                                 </a>
-                               
+                               @endif
+                               @if (auth()->user()->isAbleTo(['delete-promocode']))
 
                                 <form method="post" style="display: inline">
                                     <button type="button" onclick="performdelete('{{ $code->id }}')"
@@ -77,6 +79,7 @@
                                         </span>
                                     </button>
                                 </form>
+                                @endif
 
                         </td>
                         </tr>
