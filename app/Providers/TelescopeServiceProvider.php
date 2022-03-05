1e6ee42974b73a67c83146a47e228f1f7543aae0
islamshu12 <islamshu12@gmail.com>
2022-03-05 16:37:36 +0200

    ي

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -67,5 +67,17 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
                 'info@jooyapp.net'
             ]);
         });
+        $this->hideSensitiveRequestDetails();
+ 
+    Telescope::filter(function (IncomingEntry $entry) {
+        if ($this->app->isLocal()) {
+            return true;
+        }
+ 
+        return $entry->isReportableException() ||
+            $entry->isFailedJob() ||
+            $entry->isScheduledTask() ||
+            $entry->hasMonitoredTag();
+    });
     }
 }
