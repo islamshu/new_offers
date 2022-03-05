aa0935082a17066126a9301db4e6129d3d7d9a8f
islamshu12 <islamshu12@gmail.com>
2022-03-05 16:42:08 +0200

    ء

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -19,15 +19,16 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         // Telescope::night();
 
         $this->hideSensitiveRequestDetails();
-
+ 
         Telescope::filter(function (IncomingEntry $entry) {
-      
-
+            if ($this->app->isLocal()) {
+                return true;
+            }
+     
             return $entry->isReportableException() ||
-                   $entry->isFailedRequest() ||
-                   $entry->isFailedJob() ||
-                   $entry->isScheduledTask() ||
-                   $entry->hasMonitoredTag();
+                $entry->isFailedJob() ||
+                $entry->isScheduledTask() ||
+                $entry->hasMonitoredTag();
         });
     }
 
