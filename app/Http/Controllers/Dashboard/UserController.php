30d1e4f210261c26295343b39cd8cb2299878739
islamshu12 <islamshu12@gmail.com>
2022-03-10 16:04:21 +0200

    f

--- app/Http/Controllers/Dashboard/UserController.php
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -89,7 +89,7 @@ class UserController extends Controller
                 
                 $user->attachRole($role);
         }
-        return redirect()->bakc();
+        return redirect()->back();
         
 
   
