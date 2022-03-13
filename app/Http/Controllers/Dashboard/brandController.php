c8bdb60e0216a29076772a3ff43ce135b0ec571a
islamshu12 <islamshu12@gmail.com>
2022-03-13 13:30:22 +0200

    d

--- app/Http/Controllers/Dashboard/brandController.php
+++ app/Http/Controllers/Dashboard/brandController.php
@@ -67,9 +67,8 @@ class brandController extends Controller
     public function showmodeluser(Request $request){
         $vendor = Vendor::find($request->id);
         // $user = User::whereRoleIs('Vendors')->where('vendor_id',$request->id)->first();  
-        $users = User::whereRoleIs('Vendors')->get();
-        dd($users);
-        return view('dashboard.vendor.show_model_user')->with('vendor',$vendor)->with('user',$user);
+        
+        return view('dashboard.vendor.show_model_user')->with('vendor',$vendor);
     }
     
      public function post_cover(Request $request)
