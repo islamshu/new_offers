299d2a85ea934891850440917ad89a738af7af18
islamshu12 <islamshu12@gmail.com>
2022-03-13 10:38:28 +0200

    f

--- app/Http/Controllers/GeneralInfoController.php
+++ app/Http/Controllers/GeneralInfoController.php
@@ -31,7 +31,7 @@ class GeneralInfoController extends Controller
         $agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36';
 
         $response = Http::withUserAgent(request()->userAgent())->get('https://api.oursms.com/api-a/billing/credits?token=whyfA4pML1nN4w3Yj7_WpKDo29NIOWav-0EqK38KRco');
-
+        dd($response);
         // dd($response , request()->getHttpHost());
         // return redirect()->route('get_cridit');
         return view('dashboard.generalinfo.index');
