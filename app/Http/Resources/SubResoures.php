f89191800d55c426b3c911cbff6d5c29d66a20ad
islamshu12 <islamshu12@gmail.com>
2022-03-13 16:39:58 +0200

    d

--- app/Http/Resources/SubResoures.php
+++ app/Http/Resources/SubResoures.php
@@ -79,7 +79,7 @@ class SubResoures extends JsonResource
 
     }
     function getcridt($data){
-dd($data->type_of_subscribe);
+dd($data->expire_date);
         if($data->expire_date < Carbon::now()){
           
             $data->type_of_subscribe = 'FREE';
