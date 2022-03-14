165685d00c8b0869b7c309e934a6fe94af1f1347
islamshu12 <islamshu12@gmail.com>
2022-03-14 12:30:53 +0200

    d

--- app/Http/Resources/BranchCollection.php
+++ app/Http/Resources/BranchCollection.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class BranchCollection extends ResourceCollection
 {
+
+    
     /**
      * Transform the resource collection into an array.
      *
