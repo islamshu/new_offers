8ee08c5820479b03835d6533846695b1ad603345
islamshu12 <islamshu12@gmail.com>
2022-03-14 16:49:02 +0200

    d

--- resources/views/dashboard/discount_code/index.blade.php
+++ resources/views/dashboard/discount_code/index.blade.php
@@ -55,7 +55,8 @@
                         </td>
                         <td style="font-size: 33px">{{  $code->type_of_limit == 'unlimit' ?  '∞' : $code->number_of_code  }}</td>
                         <td style="font-size: 33px">{{ $code->type_of_limit == 'unlimit' ?  '∞' : $code->total_remain  }}</td>
-                        <td>{{ App\Models\PromocodeUser::where('code','like',$code->promocode->code)->count() }}</td>
+                        <td>{{ $code->promocode }}</td>
+                        {{-- <td>{{ App\Models\PromocodeUser::where('code','like',$code->promocode->code)->count() }}</td> --}}
                         <td class="pr-0 text-left">
 
                             <a data-toggle="modal" data-target="#myModal" class="btn btn-outline-primary"
