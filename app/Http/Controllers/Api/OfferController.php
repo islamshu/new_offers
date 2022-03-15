c755737f2fc9444a7cf347ee2ed9e249dcd2df4f
islamshu12 <islamshu12@gmail.com>
2022-03-15 14:06:51 +0200

    f

--- app/Http/Controllers/Api/OfferController.php
+++ app/Http/Controllers/Api/OfferController.php
@@ -87,10 +87,10 @@ class OfferController extends BaseController
                  }); 
                  })->where('name_ar','like','%'.$request->search_key.'%')->orWhere('name_en','like','%'.$request->search_key.'%')->where('status',1)->get();
 
-        $stores = Vendor::with('cities')->whereHas('cities', function ($q) use ($request) {
+        $stores = Vendor::has('offers')->with('cities')->whereHas('cities', function ($q) use ($request) {
             $q->where('city_id', $request->city_id);
-          })-> 
-          with('offers')->has('offers')->where('name_ar','like','%'.$request->search_key.'%')->orWhere('name_en','like','%'.$request->search_key.'%')->where('status','active')->get();
+          })
+          ->where('name_ar','like','%'.$request->search_key.'%')->orWhere('name_en','like','%'.$request->search_key.'%')->where('status','active')->get();
           // dd($stores);
         $res['data']['offers'] =  sort_offer(OfferResourses::collection($offers));
         $res['data']['stores'] =  sort_vendor(VednorResourse::collection($stores));
