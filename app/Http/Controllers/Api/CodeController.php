d77b53653a9e164cd3179bdbd48b61e208786019
islamshu12 <islamshu12@gmail.com>
2022-03-15 14:42:31 +0200

    d

--- app/Http/Controllers/Api/CodeController.php
+++ app/Http/Controllers/Api/CodeController.php
@@ -221,6 +221,7 @@ class CodeController extends BaseController
             //   dd($dd);
             $discout = @$dd->promocode;
             // $discout = DiscountSubscription::where('code',$request->code)->first();
+            dd($discout);
             if($discout != null ){
                     $sub = Subscription::find($discout->sub_id);
                     $price =$sub->price;
