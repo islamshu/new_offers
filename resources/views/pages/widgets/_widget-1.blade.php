bf5c33f1dce97826d454b6dd933096f9492f8cfc
islamshu12 <islamshu12@gmail.com>
2022-03-16 12:42:28 +0200

    d

--- resources/views/pages/widgets/_widget-1.blade.php
+++ resources/views/pages/widgets/_widget-1.blade.php
@@ -64,13 +64,19 @@
                     {{ Metronic::getSVG("media/svg/icons/General/Attachment1.svg", "svg-icon-3x svg-icon-warning d-block my-2") }}
                     <a href="#" class="text-warning font-weight-bold font-size-h6">
                         {{ __('Active Brand') }} <br>
+                        {{ App\Models\Vendor::where('enterprise_id',auth()->user()->ent_id)->where('status','active')->count() }}
                     </a>
                 </div>
                 @else
                 <div class="col-md-2 bg-light-warning px-6 py-8 rounded-xl mr-7 mb-7">
                     {{ Metronic::getSVG("media/svg/icons/General/Attachment1.svg", "svg-icon-3x svg-icon-warning d-block my-2") }}
                     <a href="#" class="text-warning font-weight-bold font-size-h6">
-                        {{ __('Active Branch') }}
+                        {{ __('Active Branch') }}<br>
+                        {{ App\Models\Branch::with('vendor')->whereHas('vendor', function ($q)  {
+                            $q->where('status','active')->where('enterprise_id',auth()->user()->ent_id);
+                        })->count();
+
+
                     </a>
                 </div>
                 @endif
