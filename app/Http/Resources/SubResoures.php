7edec1689ca62a6ef43a3eb5a9362cad41ad32ac
islamshu12 <islamshu12@gmail.com>
2022-03-16 22:05:40 +0200

    f

--- app/Http/Resources/SubResoures.php
+++ app/Http/Resources/SubResoures.php
@@ -91,6 +91,7 @@ class SubResoures extends JsonResource
         }
     }  
     function is_trial($data){
+        dd($data->is_new);
         if($data->expire_date < Carbon::now()){
             $data->type_of_subscribe = 'FREE';
             $data->save();
