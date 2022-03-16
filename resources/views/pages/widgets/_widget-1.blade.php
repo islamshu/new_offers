f3b8daf83fe98f9eb7056294f7ed80c0922943bb
islamshu12 <islamshu12@gmail.com>
2022-03-16 14:34:44 +0200

    ل

--- resources/views/pages/widgets/_widget-1.blade.php
+++ resources/views/pages/widgets/_widget-1.blade.php
@@ -124,7 +124,8 @@ $lang = app()->getLocale();
                     {{ Metronic::getSVG('media/svg/icons/Shopping/Chart-bar2.svg', 'svg-icon-3x svg-icon-warning d-block my-2') }}
                     <a href="#" class="text-warning font-weight-bold font-size-h6">
                         {{ __('Total customer saving') }} <br>
-                        0
+                        {{ App\Models\Transaction::where('enterprise_id',auth()->user()->ent_id)->sum('price') }}
+
                     </a>
                 </div>
                 <div class="col-md-2 bg-light-primary px-6 py-8 rounded-xl mr-7 mb-7">
