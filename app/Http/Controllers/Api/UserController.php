9ef75bde796cb8786644d357873766aedad6c86c
islamshu12 <islamshu12@gmail.com>
2022-03-16 21:26:18 +0200

    d

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -295,7 +295,7 @@ class UserController extends BaseController
        
         $res['status']= $this->sendResponse200('ok');
         $sub = Subscriptions_User::where('clinet_id',$user->id)->orderBy('id','desc')->first();
-        dd($user);
+        // dd($user);
         $res['data']['client_subscription'] =new UserSubscription($sub);
 
         return $res;
