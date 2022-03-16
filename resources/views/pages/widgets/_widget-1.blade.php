395d4045e0bc67b7949ba288c9c2822a9a6363b8
islamshu12 <islamshu12@gmail.com>
2022-03-16 13:55:45 +0200

    d

--- resources/views/pages/widgets/_widget-1.blade.php
+++ resources/views/pages/widgets/_widget-1.blade.php
@@ -166,7 +166,7 @@ $lang = app()->getLocale();
                 <div class="col-md-2  bg-light-danger px-6 py-8 rounded-xl mr-7 mb-7">
                     {{ Metronic::getSVG('media/svg/icons/Design/Layers.svg', 'svg-icon-3x svg-icon-danger d-block my-2') }}
                     <a href="#" class="text-danger font-weight-bold font-size-h6 mt-2">
-                        {{ __('All Transaction') }}
+                        {{ __('All Transaction') }} <br>
                         {{ App\Models\Transaction::where('enterprise_id',auth()->user()->ent_id)->count() }}
 
                     </a>
