e371f82f84c5de6b2f52f1fb6bdf463dd982e7d6
islamshu12 <islamshu12@gmail.com>
2022-03-16 13:48:33 +0200

    f

--- resources/views/pages/widgets/_widget-1.blade.php
+++ resources/views/pages/widgets/_widget-1.blade.php
@@ -129,7 +129,15 @@
                     {{ Metronic::getSVG('media/svg/icons/Communication/Outgoing-box.svg','svg-icon-3x svg-icon-primary d-block my-2') }}
                     <a href="#" class="text-primary font-weight-bold font-size-h6 mt-2">
                         {{ __('Best Brand') }} <br>
-                        {{ App\Models\Offer::find(best_offer()) }}
+                        @php
+                            $lang = app()->getLocale();
+                        @endphp
+                        @if($lang == 'ar')
+                        {{ App\Models\Offer::find(best_offer())->name_ar }}
+                        @else
+                        {{ App\Models\Offer::find(best_offer())->name_en }}
+                        @endif
+
                     </a>
                 </div>
                 <div class="col-md-2 bg-light-danger px-6 py-8 rounded-xl mr-7 mb-7">
