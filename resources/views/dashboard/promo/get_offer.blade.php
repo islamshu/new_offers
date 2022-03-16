f945b3b2e3c2cfd4fc3e4e489052d5e476aa6c03
islamshu12 <islamshu12@gmail.com>
2022-03-16 12:04:44 +0200

    f

--- resources/views/dashboard/promo/get_offer.blade.php
+++ resources/views/dashboard/promo/get_offer.blade.php
@@ -12,12 +12,15 @@
 
                 <form action="{{ route('create_offer',app()->getLocale()) }}" method="post">
                     @csrf
+                    @php
+                        $lang = app()->getLocale();
+                    @endphp
                     <div class="row">
                         <div class="col-md-3">
                             <select name="vendor_id" id="vendor_id"  class="selectpicker form-control" data-live-search="true">
                                 <option selected disabled>{{ __('choose') }}</option>
                                 @foreach ($brands as $item)
-                                    <option value="{{ $item->id }}">{{ $item->name_ar }}</option>
+                                    <option value="{{ $item->id }}">@if($lang == 'ar') {{ $item->name_ar }} @else {{ $item->name_en }} @endif </option>
                                 @endforeach
 
                             </select>
