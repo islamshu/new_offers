e42741cb5fd7298de3f5e0e04b38010f3e7bf302
islamshu12 <islamshu12@gmail.com>
2022-03-17 13:55:15 +0200

    d

--- app/Http/Middleware/Is_login.php
+++ app/Http/Middleware/Is_login.php
@@ -23,7 +23,7 @@ class Is_login
         $response['status']['HTTP_code'] = 401;
         $response['status']['HTTP_response'] = 'Unauthorized';
         $response['status']['message'] = "Unauthorized or Signed in from another device";
-        return response()->json($response , 404);
+        return response()->json($response , 401);
       }
     
         
