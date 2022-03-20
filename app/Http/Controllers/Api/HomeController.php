cc4d3eed52b63f94622d4647f9c009d9f2cbad2c
islamshu12 <islamshu12@gmail.com>
2022-03-20 09:52:03 +0200

    d

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -79,9 +79,8 @@ class HomeController extends BaseController
 
     $res['status'] = $this->sendResponse200('OK');
     $city = enterprise_city::where('enterprise_id',$ent->id)->where('status','active')->get();
-    dd($city->where('status',1));
     
-    $res['data'] = new CityCollection($city->where('status',1) );
+    $res['data'] = new CityCollection($city );
     return $res;
   }
   public function home(Request $request)
