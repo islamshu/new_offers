79f616672374e678561f9b3d496575c849503f57
islamshu12 <islamshu12@gmail.com>
2022-03-20 10:35:26 +0200

    f

--- app/Http/Controllers/Dashboard/OfferController.php
+++ app/Http/Controllers/Dashboard/OfferController.php
@@ -429,7 +429,7 @@ class OfferController extends Controller
                 $file = $request->file('primary_image');
                 $imageName = time() . 'image.' . $file->getClientOriginalExtension();
              
-                $file->move('images/primary_offer', $imageName);
+                $file->store('images/primary_offer', $imageName);
                 $image_offer->primary_image = $imageName;
                 if ($request->TotalImages > 0) {
                     $files = [];
