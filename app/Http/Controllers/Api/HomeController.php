9564ec279b0721e602d1b653d2d832b1ce8cdc6e
islamshu12 <islamshu12@gmail.com>
2022-03-21 13:19:11 +0200

    d

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -210,6 +210,8 @@ class HomeController extends BaseController
     })
     ->has('offers')->whereHas('offers', function ($q) use ($request,$city) {
       $q->where('end_time','>=',Carbon::now());
+    })->has('branches')->whereHas('branches', function ($q) use ($request,$city) {
+      $q->where('city_id',$city)->where('status','active');
     })
     ->get();
     $res['status'] = $this->sendResponse200('OK');
