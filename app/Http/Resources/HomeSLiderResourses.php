40b2a6059a75e3c872a887c01af4dd88d2751f81
islamshu12 <islamshu12@gmail.com>
2022-03-23 12:50:22 +0200

    d

--- app/Http/Resources/HomeSLiderResourses.php
+++ app/Http/Resources/HomeSLiderResourses.php
@@ -29,12 +29,9 @@ class HomeSLiderResourses extends JsonResource
         foreach($slider as $of){
             array_push($array,$of->offer_id);
         }
-
         
         return new PromostionOffer(Offer::whereIn('id',$array)->with('vendor')->whereHas('vendor', function ($q)  {
             $q->where('status', 'active');
-        })->whereHas('offerpromo', function ($q)  {
-            $q->orderBy('sort', 'asc');
         })->where('status',1)->where('end_time','>=',Carbon::now())->get());
         
 
