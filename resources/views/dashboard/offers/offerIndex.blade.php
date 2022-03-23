dd9be50f21baf05188f6daf325d5249dd3efd1ad
islamshu12 <islamshu12@gmail.com>
2022-03-23 16:10:09 +0200

    d

--- resources/views/dashboard/offers/offerIndex.blade.php
+++ resources/views/dashboard/offers/offerIndex.blade.php
@@ -129,6 +129,7 @@
 
 @section('scripts')
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
     <script src="{{ asset('crudjs/crud.js') }}"></script>
