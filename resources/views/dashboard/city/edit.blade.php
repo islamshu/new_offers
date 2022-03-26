06404ba1d9073179fc47ee8fcb7ae2e3114a99c1
islamshu12 <islamshu12@gmail.com>
2022-03-26 14:44:20 +0300

    d

--- resources/views/dashboard/city/edit.blade.php
+++ resources/views/dashboard/city/edit.blade.php
@@ -10,7 +10,7 @@ card card-docs mb-2">
             </h3>
             <ol class="breadcrumb">
                 <li><a href="/{{ get_lang() }}"><i class="fa fa-dashboard"></i> {{ __('Dashboard') }}</a></li>
-                <li><a href="{{ route('category.city',get_lang()) }}">{{ __('City') }}</a></li>
+                <li><a href="{{ route('city.index',get_lang()) }}">{{ __('City') }}</a></li>
                 <li class="active">{{ __('Edit') }}</li>
             </ol>
            
