4552b0c01501d7c92b5636e5aed6272cada2bd29
islamshu12 <islamshu12@gmail.com>
2022-03-26 16:22:03 +0300

    d

--- resources/views/dashboard/notofication/general_create.blade.php
+++ resources/views/dashboard/notofication/general_create.blade.php
@@ -8,7 +8,7 @@
         
             <ol class="breadcrumb">
                 <li><a href="/{{ get_lang() }}"><i class="fa fa-dashboard"></i> {{ __('Dashboard') }}</a></li>
-                <li><a href="{{ route('general_notofication.index',get_lang()) }}"><i class="fa fa-dashboard"></i> {{ __('Dashboard') }}</a></li>
+                <li><a href="{{ route('general_notofication.index',get_lang()) }}"><i class="fa fa-dashboard"></i> {{ __('Notofication') }}</a></li>
 
                 <li class="active">{{ __('All Notofication') }}</li>
             </ol>
