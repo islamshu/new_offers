79a045e57ddc5d1aa79ef972ad26541506f169ff
islamshu12 <islamshu12@gmail.com>
2022-03-26 23:04:42 +0300

    f

--- resources/views/dashboard/discount_code/edit.blade.php
+++ resources/views/dashboard/discount_code/edit.blade.php
@@ -4,15 +4,14 @@
     <div class="card card-custom">
 
         <div class="card-header">
-            <h3 class="card-title">
-                {{ __('Create Discount Code') }}
-            </h3>
-            <div class="card-toolbar">
-                <div class="example-tools justify-content-center">
-                    <span class="example-toggle" data-toggle="tooltip" title="View code"></span>
-                    <span class="example-copy" data-toggle="tooltip" title="Copy code"></span>
-                </div>
-            </div>
+          
+            <ol class="breadcrumb">
+                <li><a href="/{{ get_lang() }}/home"><i class="fa fa-dashboard"></i> {{ __('Dashboard') }}</a></li>
+                <li ><a href="{{ route('discount_code.index',get_lang()) }}"><i class="fa fa-dashboard"></i> {{ __('All Discount code') }}</a></li>
+                <li class="active"> {{ __('Edit Discount code') }}</li>
+        
+            </ol>
+        
         </div>
         <form class="form" method="post" id='create_form' enctype="multipart/form-data">
             @csrf
