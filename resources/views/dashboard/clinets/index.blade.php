7945b2e973e360f064ea4ca7fdc7ba1687f143f5
islamshu12 <islamshu12@gmail.com>
2022-03-26 23:12:37 +0300

    f

--- resources/views/dashboard/clinets/index.blade.php
+++ resources/views/dashboard/clinets/index.blade.php
@@ -40,6 +40,18 @@
     <div class="card card-docs mb-2">
         <div class="card-body fs-6 py-15 px-10 py-lg-15 px-lg-15 text-gray-700">
             <h2 class="mb-3">{{ __('All Users') }}</h2>
+            <div class="card-header">
+          
+               
+                <ol class="breadcrumb">
+                    <li><a href="/{{ get_lang() }}/home"><i class="fa fa-dashboard"></i> {{ __('Dashboard') }}</a></li>
+                    <li><a href="/{{ get_lang() }}/all_clients"><i class="fa fa-dashboard"></i> {{ __('All Clients') }}</a></li>
+
+                    <li class="active">{{ __('All ') }} {{ $type }} {{ 'client' }}</li>
+                </ol>
+            
+            </div> 
+    
 
             @if (Session::has('success'))
                 <div class="row mr-2 ml-2">
