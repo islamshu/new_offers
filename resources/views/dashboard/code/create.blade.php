6d2523b80dc00c49f3cee4862f8d6304043216e7
islamshu12 <islamshu12@gmail.com>
2022-03-26 23:01:57 +0300

    d

--- resources/views/dashboard/code/create.blade.php
+++ resources/views/dashboard/code/create.blade.php
@@ -7,7 +7,7 @@
           
             <ol class="breadcrumb">
                 <li><a href="/{{ get_lang() }}/home"><i class="fa fa-dashboard"></i> {{ __('Dashboard') }}</a></li>
-                <li ><a href="{{ route('code.index',get_lang()) }}"><i class="fa fa-dashboard"></i> {{ __('Dashboard') }}</a></li>
+                <li ><a href="{{ route('code.index',get_lang()) }}"><i class="fa fa-dashboard"></i> {{ __('All Activation code') }}</a></li>
                 <li class="active"> {{ __('Create Activation code') }}</li>
             </ol>
         
