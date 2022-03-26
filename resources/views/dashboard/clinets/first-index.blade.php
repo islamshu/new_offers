269ddadb0f19b88bbd0a8a6ec46295174ede15af
islamshu12 <islamshu12@gmail.com>
2022-03-26 23:09:19 +0300

    ل

--- resources/views/dashboard/clinets/first-index.blade.php
+++ resources/views/dashboard/clinets/first-index.blade.php
@@ -3,7 +3,17 @@
  
     <div class="card card-docs mb-2">
         <div class="card-body fs-6 py-15 px-10 py-lg-15 px-lg-15 text-gray-700">
-            <h2 class="mb-3">{{ __('All Clients') }}</h2>
+            <div class="card-header">
+          
+               
+                <ol class="breadcrumb">
+                    <li><a href="/{{ get_lang() }}/home"><i class="fa fa-dashboard"></i> {{ __('Dashboard') }}</a></li>
+                    
+                    <li class="active">{{ __('All Clients') }}</li>
+                </ol>
+            
+            </div> 
+    
             <div class="mt-10">
 
                 <div class="row">
