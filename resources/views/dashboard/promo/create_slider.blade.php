f01c420c74eee0b465cf87e7422c2bfa8165b4b5
islamshu12 <islamshu12@gmail.com>
2022-03-26 15:05:19 +0300

    f

--- resources/views/dashboard/promo/create_slider.blade.php
+++ resources/views/dashboard/promo/create_slider.blade.php
@@ -10,7 +10,7 @@
             <ol class="breadcrumb">
                 <li><a href="/{{ get_lang() }}"><i class="fa fa-dashboard"></i> {{ __('Dashboard') }}</a></li>
                 <li><a href="{{ route('promotion.index',get_lang()) }}"><i class="fa fa-dashboard"></i> {{ __('promotion') }}</a></li>
-                <li><a href="{{ route('get_elemet_by_type',[get_lang(),'slider', $city_id]) }}"><i class="fa fa-dashboard"></i> {{ __('banner') }}</a></li>
+                <li><a href="{{ route('get_elemet_by_type',[get_lang(),'slider', $city_id]) }}"><i class="fa fa-dashboard"></i> {{ __('Sliders') }}</a></li>
                 
                 <li class="active">{{ __('create Slider') }}</li>
             </ol>
