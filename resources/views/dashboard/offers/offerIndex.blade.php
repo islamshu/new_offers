ddd3aa28167aae5683a81c8c8690677aa097a374
islamshu12 <islamshu12@gmail.com>
2022-03-26 17:07:57 +0300

    r

--- resources/views/dashboard/offers/offerIndex.blade.php
+++ resources/views/dashboard/offers/offerIndex.blade.php
@@ -7,7 +7,7 @@
             <li><a href="/{{ get_lang() }}/home"><i class="fa fa-dashboard"></i> {{ __('Dashboard') }}</a></li>
             <li><a href="/{{ get_lang() }}/offers"><i class="fa fa-dashboard"></i> {{ __('Vendors') }}</a></li>
     
-            <li class="active">{{ __('Offers') }}</li>
+            <li class="active">{{ __('Offers') }} {{ $vendor->name_en }} </li>
         </ol>
         <div class="card-body fs-6 py-15 px-10 py-lg-15 px-lg-15 text-gray-700">
             @if (auth()->user()->isAbleTo(['create-offer']))
