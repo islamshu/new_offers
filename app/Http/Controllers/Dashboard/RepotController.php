19ac371187c00f589ed97af55eda4d5c806c58bc
islamshu12 <islamshu12@gmail.com>
2022-03-27 18:09:49 +0300

    ي

--- app/Http/Controllers/Dashboard/RepotController.php
+++ app/Http/Controllers/Dashboard/RepotController.php
@@ -184,14 +184,14 @@ class RepotController extends Controller
             }
         });
         $query->when($request->offer_status, function ($q) use ($request) {
-            return $q->where('status',$request->offer_status);
+            return $q->where('status',(int)$request->offer_status);
         });
-
-        $query->when($request->vendor_status, function ($q) use ($request) {
-            $q->whereHas('vendor', function ($qq) use ($request) {
-                return $qq->where('status', $request->vendor_status);
-              });
-        });
+        
+        // $query->when($request->vendor_status, function ($q) use ($request) {
+        //     $q->whereHas('vendor', function ($qq) use ($request) {
+        //         return $qq->where('status', $request->vendor_status);
+        //       });
+        // });
         $query->when($request->number_date, function ($q) use ($request) {
             return $q->where('end_time', Carbon::now()->addDays($request->number_date));
         });
