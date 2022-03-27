d8dbe1f1a45781f291366744a6a6132993ef1616
islamshu12 <islamshu12@gmail.com>
2022-03-27 17:59:24 +0300

    d

--- app/Http/Controllers/Dashboard/RepotController.php
+++ app/Http/Controllers/Dashboard/RepotController.php
@@ -209,11 +209,11 @@ class RepotController extends Controller
               });
         });
 
-        $offers = $query->get();
+        $offers = $query->paginate(30);
         if($request->created_to == null && $request->created_to == null && $request->offer_status == null && $request->vendor_status == null &&
         $request->number_date == null && $request->city_id == null && $request->category_id == null  )
     {
-        $offers = Offer::whereDate('created_at', Carbon::today())->get();
+        $offers = Offer::whereDate('created_at', Carbon::today())->paginate(30);
         return view('dashboard.repots.offers', compact('offers', 'request'));
 
     }else{
--- resources/views/dashboard/repots/offers.blade.php
+++ resources/views/dashboard/repots/offers.blade.php
@@ -104,7 +104,7 @@
 
 
         <table class="datatable table datatable-bordered datatable-head-custom  table-row-bordered gy-5 gs-7"
-            id="kt_datatable">
+            >
             <thead>
                 <tr class="fw-bold fs-6 text-gray-800">
 
@@ -160,6 +160,7 @@
             </tbody>
 
         </table>
+        {!! $offers->appends(request()->input())->links() !!}
 
 
     </div>
