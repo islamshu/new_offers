833a116f7342d6f9fa0ab5573a90e8bb7a2b4bec
islamshu12 <islamshu12@gmail.com>
2022-03-28 11:58:47 +0300

    ب

--- resources/views/dashboard/repots/offers.blade.php
+++ resources/views/dashboard/repots/offers.blade.php
@@ -144,7 +144,8 @@
 
 
         <table class="datatable table datatable-bordered datatable-head-custom  table-row-bordered gy-5 gs-7"
-            >
+        id="kt_datatable">
+    
             <thead>
                 <tr class="fw-bold fs-6 text-gray-800">
                     <th>{{ __('Brand name') }}</th>
