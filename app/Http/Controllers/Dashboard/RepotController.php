25f3aceed9c30d23c6f9e8a149413df5ba84715e
islamshu12 <islamshu12@gmail.com>
2022-03-30 00:46:07 +0300

    g

--- app/Http/Controllers/Dashboard/RepotController.php
+++ app/Http/Controllers/Dashboard/RepotController.php
@@ -220,7 +220,7 @@ class RepotController extends Controller
 
            $test_q= str_replace(" ", "%", $request->get('query'));
 
-            $query = Offer::query()->where('name_en','like','%'.$test_q.'%');
+            $query = Offer::query()->where('name_en','like','%'.$test_q.'%')->orWhere('name_ar','like','%'.$test_q.'%');
           
 
             $query->when($request->created_form, function ($q) use ($request) {
