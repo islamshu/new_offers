149761c1a3196918184c71a7df84e16d7a119646
islamshu12 <islamshu12@gmail.com>
2022-03-29 23:49:22 +0300

    g

--- app/Http/Controllers/Dashboard/RepotController.php
+++ app/Http/Controllers/Dashboard/RepotController.php
@@ -253,7 +253,7 @@ class RepotController extends Controller
         });
         
         $query->when($request->vendor_status, function ($q) use ($request) {
-            $q->whereHas('vendor', function ($qq) use ($request) {
+            $q->with('vendor', function ($qq) use ($request) {
                 return $qq->where('status', $request->vendor_status);
               });
         });
