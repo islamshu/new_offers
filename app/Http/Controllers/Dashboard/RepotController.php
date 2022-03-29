9753b3a163b9ede05c74ef2032aa6d82a8d5357d
islamshu12 <islamshu12@gmail.com>
2022-03-30 00:23:07 +0300

    d

--- app/Http/Controllers/Dashboard/RepotController.php
+++ app/Http/Controllers/Dashboard/RepotController.php
@@ -223,7 +223,10 @@ class RepotController extends Controller
             $query = Offer::query();
             $query->when($request->query, function ($q) use ($test_q)
             {
-                return    $q->where('name_en','like','%'.$test_q.'%');
+                return    $q->where('name_en','like','%'.$test_q.'%')
+                ->oRwhereHas('vendor', function ($qq) use ($test_q) {
+                    return $qq->where('name_en','like','%'.$test_q.'%');
+                });
             });
 
 
@@ -251,11 +254,7 @@ class RepotController extends Controller
                     return $qq->where('status', $request->vendor_status);
                 });
             });
-            $query->when($request->query, function ($q) use ($test_q) {
-                $q->whereHas('vendor', function ($qq) use ($test_q) {
-                    return $qq->where('name_en','like','%'.$test_q.'%');
-                });
-            });
+          
             $query->when($request->number_date, function ($q) use ($request) {
                 return $q->whereDate('end_time', [Carbon::now()->addDays($request->number_date)->format('Y-m-d') . ' 00:00:00', Carbon::now()->addDays($request->number_date)->format('Y-m-d') . ' 23:59:59']);
             });
