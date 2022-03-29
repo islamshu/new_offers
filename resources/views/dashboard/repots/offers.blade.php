28d42bcba7523ca058b9559b53c4796169da8772
islamshu12 <islamshu12@gmail.com>
2022-03-29 12:44:21 +0300

    s

--- resources/views/dashboard/repots/offers.blade.php
+++ resources/views/dashboard/repots/offers.blade.php
@@ -268,10 +268,10 @@ $('#vendor_id').on('change', function() {
     });
     function fetch_data(page,  query,request)
             {
-            let  data =   $("#addUserForm").serialize();
+            let  request =   $("#addUserForm").serialize();
         
             $.ajax({
-            url:"/en/offer_reports_fetch_data?page="+page+"&query="+query+data,
+            url:"/en/offer_reports_fetch_data?page="+page+"&query="+query+"&"+request,
             success:function(data)
             {
                 
