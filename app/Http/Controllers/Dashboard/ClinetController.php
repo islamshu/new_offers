13cd929cf3fb8a4096f4ac88c12d5190856dea16
islamshu12 <islamshu12@gmail.com>
2022-03-30 15:01:14 +0300

    f

--- app/Http/Controllers/Dashboard/ClinetController.php
+++ app/Http/Controllers/Dashboard/ClinetController.php
@@ -21,7 +21,7 @@ class ClinetController extends Controller
 
     public function editedit()
     {
-        $clients =Clinet::where('expire_date','<',Carbon::now())->get();
+        $clients =Clinet::where('expire_date','<',Carbon::now())->count();
         foreach($clients as $userr)
         dd($userr);
           dd('dd');
