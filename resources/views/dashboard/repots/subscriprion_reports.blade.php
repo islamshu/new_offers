665edc1177c3a73617bbf80c3e7917b7daf4c724
islamshu12 <islamshu12@gmail.com>
2022-03-30 16:32:19 +0300

    d

--- resources/views/dashboard/repots/subscriprion_reports.blade.php
+++ resources/views/dashboard/repots/subscriprion_reports.blade.php
@@ -127,8 +127,7 @@
         <h2 class="mb-3">{{ __('All Clients') }}</h2>
 
 
-        <table class="datatable table datatable-bordered datatable-head-custom  table-row-bordered gy-5 gs-7"
-            id="kt_datatablenew">
+        <table class="datatable table datatable-bordered datatable-head-custom ">
             <thead>
                 <tr class="fw-bold fs-6 text-gray-800">
 
