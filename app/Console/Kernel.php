2c59f21a5b2944e12f71a88876b7d618d27fa5c6
islamshu12 <islamshu12@gmail.com>
2022-03-30 15:36:10 +0300

    f

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'App\Console\Commands\CheckStatus'
+        Commands\CheckStatus::class,
+
     ];
 
     /**
@@ -25,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('telescope:prune')->everyFourHours();
-        $schedule->command('\App\Console\Commands\CheckStatus')->everyTenMinutes();
+        $schedule->command('check:status')->everyFiveMinutes();
 
     }
 
