9c2f16c4c001f4ef5ab7234b379738e514e8ac97
islamshu12 <islamshu12@gmail.com>
2022-03-31 13:45:14 +0300

    f

--- resources/views/dashboard/repots/subscriprion_reports.blade.php
+++ resources/views/dashboard/repots/subscriprion_reports.blade.php
@@ -1,56 +1,57 @@
 @extends('layout.default')
 
 @section('content')
-@php
+    @php
     $lang = app()->getLocale();
-@endphp
-<div class="card card-custom">
+    @endphp
+    <div class="card card-custom">
 
-    <div class="card-header">
-        <h3 class="card-title">
-            {{ __('Slaes Repots') }}
-        </h3>
-        <div class="card-toolbar">
-            <div class="example-tools justify-content-center">
-                <span class="example-toggle" data-toggle="tooltip" title="View code"></span>
-                <span class="example-copy" data-toggle="tooltip" title="Copy code"></span>
+        <div class="card-header">
+            <h3 class="card-title">
+                {{ __('Slaes Repots') }}
+            </h3>
+            <div class="card-toolbar">
+                <div class="example-tools justify-content-center">
+                    <span class="example-toggle" data-toggle="tooltip" title="View code"></span>
+                    <span class="example-copy" data-toggle="tooltip" title="Copy code"></span>
+                </div>
             </div>
         </div>
-    </div>
-    <form method="get" action="{{route('subscriprion_reports',['locale'=>app()->getLocale()])}}">
-        
-        @if ($errors->any())
-        <div class="alert alert-danger" role="alert">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
-        @if (session()->has('message'))
-        <div class="alert {{session()->get('status')}} alert-dismissible fade show" role="alert">
-            <span> {{ session()->get('message') }}<span>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-        </div>
-        @endif
-        <div class="card-body">
-            <div class="container">
+        <form method="get" action="{{ route('subscriprion_reports', ['locale' => app()->getLocale()]) }}">
 
-                <div class="row">
-    
-    
-                  
-                       
+            @if ($errors->any())
+                <div class="alert alert-danger" role="alert">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            @if (session()->has('message'))
+                <div class="alert {{ session()->get('status') }} alert-dismissible fade show" role="alert">
+                    <span> {{ session()->get('message') }}<span>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                </div>
+            @endif
+            <div class="card-body">
+                <div class="container">
+
+                    <div class="row">
+
+
+
+
                         <div class="input-group col-md-5 mb-3">
                             <div class="input-group-prepend">
                                 <label class="input-group-text" for="inputGroupSelect01">
                                     Subscribe at
                                 </label>
                             </div>
-                          <input type="date" value="{{ $request->date_from}}" placeholder="Subscribe at" class="form-control" name="date_from">
+                            <input type="date" value="{{ $request->date_from }}" placeholder="Subscribe at"
+                                class="form-control" name="date_from">
                         </div>
                         <div class="input-group col-md-5 mb-3">
                             <div class="input-group-prepend">
@@ -58,54 +59,72 @@
                                     Subscribe To
                                 </label>
                             </div>
-                          <input type="date" value="{{ $request->date_to}}" placeholder="Subscribe To" class="form-control" name="date_to">
+                            <input type="date" value="{{ $request->date_to }}" placeholder="Subscribe To"
+                                class="form-control" name="date_to">
                         </div>
-                  
+
                         <button type="submit" class="btn btn-primary mr-2">Submit</button>
 
                     </div>
-              
+
+                </div>
+
             </div>
 
-        </div>
-        
-    </form>
-</div>
-<div class="card card-docs mb-2">
-    <div class="card-body fs-6 py-15 px-10 py-lg-15 px-lg-15 text-gray-700">
-        <h2 class="mb-3">{{ __('All Clients') }}</h2>
+        </form>
+    </div>
+    <div class="card card-docs mb-2">
+        <div class="card-body fs-6 py-15 px-10 py-lg-15 px-lg-15 text-gray-700">
+            <h2 class="mb-3">{{ __('All Clients') }}</h2>
 
 
-        <table class="datatable table datatable-bordered datatable-head-custom " style="width: 50%">
-            <thead>
-                <tr class="fw-bold fs-6 text-gray-800">
-
-                    <th>{{ __('Trial') }}</th>
-                    <td>{{ $trial }}</td>
-                </tr>
-                <tr>
-                    <th>{{ __('Activation Code') }}</th>
-                    <td>{{ $activation }}</td>
-                </tr>
-                <tr>
-                    <th>{{ __('Admin') }}</th>
-                    <td>{{ $admin }}</td>
-                </tr>
-                <tr>
-                    <th>{{ __('Visa') }}</th>
-                    <td>{{ $visa }}</td>
-                </tr>
+            <table class="datatable table datatable-bordered datatable-head-custom " style="width: 50%">
+                <thead>
+                    <tr class="fw-bold fs-6 text-gray-800">
+                        <th>{{ __('Trial') }}</th>
+                        <td>{{ $trial }}</td>
+                    </tr>
+                    <tr>
+                        <th>{{ __('By Admin') }}</th>
+                        <td>{{ $admin }}</td>
+                    </tr>
+                   <tr>
                     <th>{{ __('By Excel') }}</th>
                     <td>{{ $excel }}</td>
-                </tr>
-            </thead>
-         
+                    </tr>
+                    <tr>
+                        <th>{{ __('Activation Code') }}</th>
+                        <td>{{ $activation }}</td>
+                    </tr>
 
-        </table>
+                    <tr>
+                        <th>{{ __('Visa/Master') }}</th>
+                        <td>{{ $visa }}</td>
+                    </tr>
+                    <tr>
+                        <th>{{ __('STC') }}</th>
+                        <td>{{ $visa }}</td>
+                    </tr>
+                    <tr>
+                        <th>{{ __('Apple pay') }}</th>
+                        <td>{{ $visa }}</td>
+                    </tr>
+                    <tr>
+                        <th>{{ __('Apple pay (Mada)') }}</th>
+                        <td>{{ $visa }}</td>
+                    </tr>
+                    <tr>
+                        <th>{{ __('Mada') }}</th>
+                        <td>{{ $visa }}</td>
+                    </tr>
+                </thead>
 
 
+            </table>
+
+
+        </div>
     </div>
-</div>
 
 
 
@@ -113,51 +132,51 @@
 @endsection
 
 @section('scripts')
-<script>
-    $(document).ready(function() {
+    <script>
+        $(document).ready(function() {
 
-$('#vendor_id').on('change', function() {
-    // console.log("hmm its change");
-    var cat_id = $(this).val();
-    // console.log(cat_id);
-    var div = $(this).parent();
+            $('#vendor_id').on('change', function() {
+                // console.log("hmm its change");
+                var cat_id = $(this).val();
+                // console.log(cat_id);
+                var div = $(this).parent();
 
-    var op = " ";
+                var op = " ";
 
-    $.ajax({
-        type: 'get',
-        url: "{{ route('get_branch_ajax', ['locale' => app()->getLocale()]) }}",
-        data: {
-            'venodr_id': cat_id
-        },
-        success: function(data) {
-            $('#branch_id').html(new Option('', '','disabled','selected'));
-            for (var i = 0; i < data.length; i++) {
-                @if($lang == 'ar')
-                $('#branch_id').append(new Option(data[i].name_ar,
-                    data[i].id));
-                    @else
-                    $('#branch_id').append(new Option(data[i].name_en,
-                    data[i].id));
-                    @endif
+                $.ajax({
+                    type: 'get',
+                    url: "{{ route('get_branch_ajax', ['locale' => app()->getLocale()]) }}",
+                    data: {
+                        'venodr_id': cat_id
+                    },
+                    success: function(data) {
+                        $('#branch_id').html(new Option('', '', 'disabled', 'selected'));
+                        for (var i = 0; i < data.length; i++) {
+                            @if ($lang == 'ar')
+                                $('#branch_id').append(new Option(data[i].name_ar,
+                                data[i].id));
+                            @else
+                                $('#branch_id').append(new Option(data[i].name_en,
+                                data[i].id));
+                            @endif
 
-            }
-        },
-        error: function() {
+                        }
+                    },
+                    error: function() {
 
-        }
-    });
+                    }
+                });
 
-});
-});
-</script>
-<script>
-    $(function() {
-        $("#kt_datatablenew").DataTable({
-            // "pagingType": "full_numbers",
-            "scrollX": true
-
+            });
         });
-    });
-</script>
+    </script>
+    <script>
+        $(function() {
+            $("#kt_datatablenew").DataTable({
+                // "pagingType": "full_numbers",
+                "scrollX": true
+
+            });
+        });
+    </script>
 @endsection
