ab77411b67bc2dcb52dbab3c285b9b7f9f306a43
islamshu12 <islamshu12@gmail.com>
2022-04-02 12:05:29 +0300

    ر

--- config/laravel-fcm.php
+++ config/laravel-fcm.php
@@ -7,6 +7,6 @@ return [
      * Change to yours
      */
 
-    'server_key' => env('AAAA6evb7Fo:APA91bHLRf_AcHVCtgiwbjnO_mxEaAXfd4798oEyBNOUvBm5V-ve3xd86dr98h2XkJeH73Oi7pXbbI0O-hYduZgoiG7Fz1CDRRljcidQfSKV-4aNizl_hSUxTlHWjSJvn0_I7GoTBNX4'),
+    'server_key' => env('FCM_SERVER_KEY', ''),
 
 ];
