eee32c622c3cdda52d54984c58d391718535323a
islamshu12 <islamshu12@gmail.com>
2022-04-02 15:35:11 +0300

    d

--- app/Http/Controllers/Dashboard/ClinetController.php
+++ app/Http/Controllers/Dashboard/ClinetController.php
@@ -179,7 +179,7 @@ class ClinetController extends Controller
             
             }elseif($type == 'premiumuser'){
                 
-                $clinets =   Clinet::where('type_of_subscribe','PREMIUM')->paginate(20);
+                $clinets =   Clinet::where('type_of_subscribe','PREMIUM')->where('name','like', '%'.$query.'%')->paginate(20);
                 // ->orWhere('name', 'like', '%'.$query.'%')
                 // ->orWhere('email', 'like', '%'.$query.'%')
                 // ->orWhere('phone','like', '%'.$query.'%')
