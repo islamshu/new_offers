31e31e941a6b5653cf4fd12779081a889686047c
islamshu12 <islamshu12@gmail.com>
2022-04-03 11:13:09 +0300

    d

--- app/Http/Controllers/Dashboard/ClinetController.php
+++ app/Http/Controllers/Dashboard/ClinetController.php
@@ -186,6 +186,7 @@ class ClinetController extends Controller
                     return $q->whereBetween('register_date',[$request->regestar_from,Carbon::now()]);
                 }
                 if($request->regestar_from == $request->regestar_to  ){
+                    dd('d');
                     return $q->whereBetween('register_date',[$request->regestar_from . ' 00:00:00', $request->regestar_from . ' 23:59:59']);
                 }
             });
