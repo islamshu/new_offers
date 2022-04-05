7ea33afba94d08e5973f09f4cd215ec999da91bc
islamshu12 <islamshu12@gmail.com>
2022-04-05 16:02:06 +0300

    f

--- app/Http/Controllers/Dashboard/OfferController.php
+++ app/Http/Controllers/Dashboard/OfferController.php
@@ -318,7 +318,7 @@ class OfferController extends Controller
 
             return response()->json(['icon' => 'success', 'title' => 'offer created successfully'], $offer ? 200 : 400);
         } else {
-           foreach($validator->getMessageBag() as $key=>$err){
+           foreach($validator->getMessageBag()->all() as $key=>$err){
                dd($key);
            }
             return response()->json(['icon' => 'error', 'title' => $validator->getMessageBag()->first()], 400);
