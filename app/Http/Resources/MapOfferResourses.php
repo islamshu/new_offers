a0a95c286b67f012d18d2b280c48447c5720349e
islamshu12 <islamshu12@gmail.com>
2022-04-05 16:22:20 +0300

    ي

--- app/Http/Resources/MapOfferResourses.php
+++ app/Http/Resources/MapOfferResourses.php
@@ -39,9 +39,7 @@ class MapOfferResourses extends JsonResource
     public function get_distance($data,$request){
         $array =[];
         $branches = Branch::where('status','active')->where('vendor_id',$data->vendor_id)->get();
-        dd($request->latitude,$request->longitude);
         foreach($branches as $branch){
-            
            $value =  get_dinstance(@$request->latitude,@$request->longitude,@$branch->latitude,@$branch->longitude);
             $di = $value  * 1.609344 ;
             array_push($array,$di);
