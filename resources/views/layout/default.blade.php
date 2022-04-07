64648c3f110f7077599fed3789714f8f84de784f
islamshu12 <islamshu12@gmail.com>
2022-04-07 15:50:05 +0300

    d

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -15,9 +15,9 @@
     {{-- Favicon --}}
     <link rel="shortcut icon" href="{{ asset('media/logos/favicon.ico') }}" />
     <link href="{{ asset('/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
-
+    {{-- <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css"> --}}
+    <link rel="stylesheet" href="{{ asset('css/select.css') }}">
     {{-- Fonts --}}
     {{ Metronic::getGoogleFontsInclude() }}
 
