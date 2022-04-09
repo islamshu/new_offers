3819e60b965d8f4a030c3fd458235ca7777382c6
islamshu12 <islamshu12@gmail.com>
2022-04-09 17:16:34 +0300

    f

--- app/Http/Controllers/Dashboard/GeneralNotoficationController.php
+++ app/Http/Controllers/Dashboard/GeneralNotoficationController.php
@@ -143,7 +143,7 @@ class GeneralNotoficationController extends Controller
         ];
 
        
-        $users = Clinet::whereIn('id',$request->user_id)->get();
+        $users = Clinet::whereIn('id',json_decode($request->user_id))->get();
         return $users;
         foreach($users as $user){
             Notification::send($user, new UserNotification($date));
