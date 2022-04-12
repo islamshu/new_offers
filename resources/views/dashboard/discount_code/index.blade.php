c3d62665b1e75d7be793de8244d018b8cba058c6
islamshu12 <islamshu12@gmail.com>
2022-04-12 15:13:39 +0300

    ي

--- resources/views/dashboard/discount_code/index.blade.php
+++ resources/views/dashboard/discount_code/index.blade.php
@@ -13,7 +13,7 @@
             </div>
 
             <table class="datatable table datatable-bordered datatable-head-custom  table-row-bordered gy-5 gs-7"
-                id="kt_datatable" style="overflow-x:auto;">
+                id="kt_datatable" style="overflow-y:auto;">
                 <thead>
                     <tr class="fw-bold fs-6 text-gray-800">
 
