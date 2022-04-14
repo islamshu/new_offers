707fd956530d14a2b72b4251324c0b022c2e74b8
islamshu12 <islamshu12@gmail.com>
2022-04-14 16:02:10 +0300

    d

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -418,8 +418,8 @@ class HomeController extends BaseController
       
       $res['status'] = $this->sendResponse200('OK');
       $array = [];
-      $pops = PopupUser::find($data_show)->where('client_id', auth('client_api')->id())->first();
-      if($pops && $data_show->num_show == 'once' ){
+      $pops = PopupUser::find($data_show->id)->where('client_id', auth('client_api')->id())->first();
+      if($pops  && $data_show->num_show == 'once' ){
         $res['status'] = $this->SendError('OK');
       }
     
