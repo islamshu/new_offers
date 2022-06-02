b51f0bf74a53bdc0211c439f36b5b3bd46bd4bd3
islamshu12 <islamshu12@gmail.com>
2022-06-02 20:27:27 +0300

    f

--- app/Http/Controllers/Api/PayemntController.php
+++ app/Http/Controllers/Api/PayemntController.php
@@ -596,7 +596,7 @@ class PayemntController extends BaseController
     }
     public  function active2(Request $request)
     {
-        $payment = Payment::where('order_id',(string)$request->order_id)->where('invoice_id',(int)$request->invoice_id)->first();
+        $payment = Payment::where('order_id',(int)$request->order_id)->where('invoice_id',(string)$request->invoice_id)->first();
         
         if(!$payment){
             $pa = new Falid_payments();
